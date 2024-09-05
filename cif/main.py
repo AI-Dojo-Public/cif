a78a42dc59345ab8f4c9acbbb3feea7e7007b5d8
Jiri Raja <raja@ics.muni.cz>
2024-09-05 14:31:10 +0200

    Added support for firehole's config; Added support for custom env variables; Added support for custom repository

--- cif/main.py
+++ cif/main.py
@@ -8,31 +8,16 @@ from argparse import ArgumentParser
 PATH_BASE = os.path.dirname(__file__).rsplit("/", 1)[0]
 PATH_IMAGES = os.path.join(PATH_BASE, "images")
 PATH_BUILD = os.path.join(PATH_BASE, ".build")
-GITLAB_REPOSITORY = "repository/"
 
 docker_client = from_env()
 
 
-def update_firehole_config():
-    # TODO: needs to be dynamic depending on the present services
-    config = {
-        "applications": [
-            {
-                "type": "http_s",
-                "host": "0.0.0.0",
-                "port": 80,
-                "origin_host": "127.0.0.1",
-                "origin_port": 8000,
-                "vulnerabilities": ["cve-2016-10073"]
-            }
-        ]
-    }
-
-
-def copy_image(build_id: str, image_name: str) -> str:
+def copy_image(build_id: str, image_name: str, firehole_config: str) -> str:
     image_directory: str = os.path.join(PATH_IMAGES, image_name)
     tmp_image_directory: str = os.path.join(PATH_BUILD, build_id, image_name)
     shutil.copytree(image_directory, tmp_image_directory)
+    if firehole_config:
+        shutil.copyfile(firehole_config, os.path.join(tmp_image_directory, "config.yml"))
 
     return tmp_image_directory
 
@@ -54,33 +39,38 @@ def update_image(image_directory: str, image_name: str, previous_tag: str):
         dockerfile.writelines(lines)
 
 
-def build_image(image_directory: str, image_tag: str):
-    docker_client.images.build(path=image_directory, tag=image_tag)
+def build_image(image_directory: str, image_tag: str, variables_override: list[str]):
+    build_args: dict[str, str] = dict([variable.split("=", 1) for variable in variables_override])
+    docker_client.images.build(path=image_directory, tag=image_tag, buildargs=build_args)
 
 
-def image_pipeline(build_id: str, image_name: str, previous_tag: str) -> str:
+def image_pipeline(repository: str, build_id: str, image_name: str, previous_tag: str, variables_override: list[str], firehole_config: str) -> str:
     previous_tag_name = previous_tag.rsplit("/", 1)[1] + "_" if previous_tag else ""
-    image_tag = f"{GITLAB_REPOSITORY}{previous_tag_name}{image_name.replace('_', '')}"
-    image_directory = copy_image(build_id, image_name)
-    if image_name == "firehole":
-        update_firehole_config()
+    image_tag = f"{repository}{'' if repository.endswith('/') else '/'}{previous_tag_name}{image_name.replace('_', '')}"
+    image_directory = copy_image(build_id, image_name, firehole_config if image_name == "firehole" else "")
     update_image(image_directory, image_name, previous_tag)
-    build_image(image_directory, image_tag)
+    build_image(image_directory, image_tag, variables_override)
 
     return image_tag
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("args", nargs="*")
-    required_images = parser.parse_args().args
+    parser.add_argument("images", nargs="*")
+    parser.add_argument("-v", "--variable", action='append')
+    parser.add_argument("-fc", "--firehole-config")
+    parser.add_argument("-r", "--repository", default="repository")
+    required_images = parser.parse_args().images
+    variables_override = parser.parse_args().variable
+    firehole_config = parser.parse_args().firehole_config
+    image_repository = parser.parse_args().repository
 
     build_id = str(uuid1())
     images: list[str] = ["_base"] + required_images
     image_tags: list[str] = list()
     for image in images:
         print(f"Building image {image}.. ")
-        tag = image_pipeline(build_id, image, image_tags[-1] if len(image_tags) > 0 else "")
+        tag = image_pipeline(image_repository, build_id, image, image_tags[-1] if len(image_tags) > 0 else "", variables_override, firehole_config)
         image_tags.append(tag)
         print("Success.")
         print(f"Tagged as {tag}\n")
