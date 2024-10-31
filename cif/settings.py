5cd0235208eba6a21a9411f50ed3b912583b233a
Jiri Raja <raja@ics.muni.cz>
2024-10-31 14:24:26 +0100

    Fix: fixed inconsistent installation of services/actions for wheel/dist

--- cif/settings.py
+++ cif/settings.py
@@ -1,7 +1,7 @@
 import os
 
 
-PATH_BASE = os.path.dirname(__file__).rsplit("/", 1)[0]
+PATH_BASE = os.path.dirname(__file__)
 PATH_SERVICES = os.path.join(PATH_BASE, "services")
 PATH_ACTIONS = os.path.join(PATH_BASE, "actions")
 PATH_BUILD = os.path.join(PATH_BASE, ".build")
