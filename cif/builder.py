783d0fccb414a7f43ef244bc9d5a320ca61d56b0
Jiří Rája <469429@mail.muni.cz>
2024-11-27 08:48:14 +0100

    Resolve "Add option to add files"

--- cif/builder.py
+++ cif/builder.py
@@ -4,16 +4,13 @@ from uuid import uuid1
 import subprocess
 
 from cif.settings import PATH_SERVICES, PATH_ACTIONS, PATH_BUILD
-from cif.helpers import check_for_forbidden_services
+from cif.helpers import check_for_forbidden_services, FileCopy
 
 
-def copy_definition(build_id: str, image_name: str, additional_files: list[tuple[str, str]] = None) -> str:
+def copy_definition(build_id: str, image_name: str) -> str:
     image_directory: str = os.path.join(PATH_SERVICES, image_name)
     tmp_image_directory: str = os.path.join(PATH_BUILD, build_id, image_name)
     shutil.copytree(image_directory, tmp_image_directory)
-
-    for file_path, file_new_name in additional_files if additional_files else []:
-        shutil.copyfile(file_path, os.path.join(tmp_image_directory, file_new_name))
 
     return tmp_image_directory
 
@@ -67,44 +64,51 @@ def remove_partial_build_images(build_id: str):
         )
 
 
-def build_final_docker_image(image_tag: str, previous_tag: str, build_id: str):
+def create_tmp_files(files: list[FileCopy], image_directory: str) -> list[str]:
+    tmp_files_directory: str = os.path.join(image_directory, "files")
+    os.mkdir(tmp_files_directory)
+    instructions: list[str] = list()
+
+    for host_path, image_path, user, group, mode in files:
+        destination: str = shutil.copy2(host_path, tmp_files_directory)
+        context_destination = destination.removeprefix(image_directory)
+        chmod = f"--chmod={mode}" if mode else ""
+        chown = f"--chown={user if user else ''}{f':{group}' if group else ''}"
+        instructions.append(f"COPY {chown} {chmod} {context_destination} {image_path}\n")
+
+    return instructions
+
+
+def build_final_docker_image(image_tag: str, previous_tag: str, build_id: str, files: list[FileCopy]):
     labels = [f"cif_build={build_id}", "build=final"]
     image_directory: str = os.path.join(PATH_BUILD, build_id, "final")
     os.mkdir(image_directory)
-    with open(os.path.join(image_directory, "Dockerfile"), "w") as f:
-        f.write(f"FROM {previous_tag} AS {image_tag}")
+    copy_files_instructions = create_tmp_files(files, image_directory)
+
+    with open(os.path.join(image_directory, "Dockerfile"), "w") as docker_file:
+        docker_file.write(f"FROM {previous_tag} AS {image_tag}\n")
+        docker_file.writelines(copy_files_instructions)
     build_docker_image(image_directory, image_tag, {}, labels)
 
 
 def image_pipeline(
-    build_id: str,
-    image_name: str,
-    previous_tag: str,
-    variables: dict[str, str],
-    additional_files: list[tuple[str, str]],
-    packages: list[str],
+    build_id: str, image_name: str, previous_tag: str, variables: dict[str, str], packages: list[str]
 ) -> str:
     previous_tag_name = previous_tag.rsplit("/", 1)[1] + "_" if previous_tag else ""
     image_tag = f"{build_id}/{previous_tag_name}{image_name.replace('_', '')}"
     labels = [f"cif_build={build_id}", "build=partial"]
-    image_directory = copy_definition(build_id, image_name, additional_files)
+    image_directory = copy_definition(build_id, image_name)
     update_dockerfile(image_directory, image_name, previous_tag, packages)
     build_docker_image(image_directory, image_tag, variables, labels)
 
     return image_tag
 
 
-def build_services(
-    build_id: str, required_images: list[str], variables: dict[str, str], firehole_config: str, packages: list[str]
-) -> list[str]:
-    services: list[str] = ["_base"] + required_images
-    if firehole_config:
-        services.append("_firehole")
+def build_services(build_id: str, services: list[str], variables: dict[str, str], packages: list[str]) -> list[str]:
     image_tags: list[str] = list()
     for service in services:
-        additional_files = [(firehole_config, "config.yml")] if service == "_firehole" else []
         previous_tag = image_tags[-1] if image_tags else ""
-        tag = image_pipeline(build_id, service, previous_tag, variables, additional_files, packages)
+        tag = image_pipeline(build_id, service, previous_tag, variables, packages)
         image_tags.append(tag)
 
     return image_tags
@@ -143,33 +147,50 @@ def build(
     variables: dict[str, str],
     actions: list[tuple[str, dict[str, str]]],
     firehole_config: str,
-    image_tag: str,
+    final_tag: str,
     packages: list[str],
+    files: list[FileCopy],
     clean_up: bool = True,
 ) -> list[str]:
     """
     Build image containing the defined services and actions.
+
+    The `files` parameter takes:
+    host_path: File path on the host system
+    image_path: File path on the image
+    username: username or UID to change as the owner of the file (uses Docker's chown)
+    groupname: groupname or GID to change as the owner of the file (uses Docker's chown)
+    mode: Set file permission bits (uses Docker's chmod)
+    More information can be found in the Docker documentation https://docs.docker.com/reference/dockerfile/#copy.
+
     :param services: Services to add to the final image
     :param variables: Build arguments passed to the docker builder
     :param actions: Actions (and their variables) to add to the final image
     :param firehole_config: Config for Firehole, otherwise it won't run
-    :param image_tag: Tag of the final image
+    :param final_tag: Tag of the final image
     :param packages: Packages to add to the final image
-    :param clean_up: Remove all images other than the final one
+    :param files: Files to add to the final image (host_path, image_path, username, groupname, mode)
+    :param clean_up: Remove all artifacts from the build (tmp images, tmp files)
     :return: All built image tags
     """
     if forbidden_services := check_for_forbidden_services(services):
         raise ValueError(f"Services {forbidden_services} are forbidden.")
 
+    services = ["_base"] + services
+    if firehole_config:
+        services.append("_firehole")
+        files.append((firehole_config, "/firehole-config.yml", None, None, None))
+
     build_id = str(uuid1().fields[0])
-    image_tags = build_services(build_id, services, variables, firehole_config, packages)
+    image_tags = build_services(build_id, services, variables, packages)
     image_tags += perform_actions(build_id, image_tags[-1], actions)
 
-    build_final_docker_image(image_tag, image_tags[-1], build_id)
-    image_tags.append(image_tag)
+    build_final_docker_image(final_tag, image_tags[-1], build_id, files)
+    image_tags.append(final_tag)
 
     if clean_up:
         remove_partial_build_images(build_id)
-        return [image_tag]
+        shutil.rmtree(os.path.join(PATH_BUILD, build_id))
+        return [final_tag]
 
     return image_tags
